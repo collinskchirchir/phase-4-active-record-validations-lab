6a02b608eac0d8cdfac5704dc1ac48821a9ffebf
Collins Chirchir <collinskchirchir@gmail.com>
2023-03-19 14:08:53 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  
+  # Custom validation
+  validate :title_should_contain
+
+  TITLE_NOT_CONTAIN = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top/i,
+    /Guess/i
+
+  ]
+  def title_should_contain
+      if TITLE_NOT_CONTAIN.none? { |word| word.match title}
+          errors.add(:title, "Title not clickbaity enough")
+      end
+  end
+
 end
